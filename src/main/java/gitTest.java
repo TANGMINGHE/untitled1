28d82bc7a2dca362f61353a970bd7a35b2866313
tanghe <tanghe@atguigu.com>
2022-06-19 12:56:39 +0800

    5.0.0

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -2,6 +2,6 @@ public class gitTest {
     public static void main(String[] args){
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("ksld");
+        System.out.println("ksldxcxxxxxxxxxxxxxxxxxxxd");
     }
 }
